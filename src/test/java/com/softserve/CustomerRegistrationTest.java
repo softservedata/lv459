5d1041cfbc3a2fc6eb917a79ec801024cb5e8a82
serhiy2000 <3774217@gmail.com>
2019-12-17 22:57:49 +0200

    tests updated

--- src/test/java/com/softserve/CustomerRegistrationTest.java
+++ src/test/java/com/softserve/CustomerRegistrationTest.java
@@ -1,35 +1,59 @@
 package com.softserve;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class NewCustomerRegistration {
+public class CustomerRegistrationTest {
 
+  private static WebDriver driver;
+
+  @BeforeClass
+  public static void startDriver() throws Exception {
+    System.out.println("@BeforeClass-Start Driver");
+
+   //    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    System.setProperty("webdriver.chrome.driver", this.getClass().getResource("chromedriver*").getPath());
+
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  }
+
+  @AfterClass
+  public static void stopDriver() throws Exception {
+    System.out.println("@AfterClass - Stop Driver");
+    Thread.sleep(2000);
+    driver.quit();
+  }
+
+  @Before
+  public void pageLoad(){
+    System.out.println("@Before method - Loading Page");
+    driver.get("http://172.16.0.128/opencart/upload/");
+  }
+
+  @After
+  public void stopPage(){
+    System.out.println("@After method - Stop Page");
+  }
 
 
   @Test
-  public void newCustomerRegistration() throws Exception {
+  public void getEnvironment(){
+    System.out.println(System.getenv("USER_EMAIL"));
+  }
+  @Test
+  public void newCustomerRegistrationTest() throws Exception {
 
-    //    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    //    System.setProperty("webdriver.chrome.driver",
-    //                       this.getClass().getResource(Reg
-    //                                                   "chromedriver*").getPath());
-    //
-
-    WebDriver driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.get("http://172.16.0.128/opencart/upload/");
-
-    driver.findElement(By.className("dropdown")).click(); // a[title="My
-    // account"]
+    driver.findElement(By.className("dropdown")).click();
     //    Thread.sleep(1000);
     driver.findElement(By.linkText("Register")).click(); // a[href*=]
 
@@ -41,7 +65,7 @@ public class NewCustomerRegistration {
     driver.findElement(By.id("input-lastname")).sendKeys("Pe");
 
     driver.findElement(By.id("input-email")).clear();
-    driver.findElement(By.id("input-email")).sendKeys("lv459@i.ua");
+    driver.findElement(By.id("input-email")).sendKeys(System.getenv("USER_EMAIL"));
 
     driver.findElement(By.id("input-telephone")).clear();
     driver.findElement(By.id("input-telephone")).sendKeys("123");
@@ -51,7 +75,6 @@ public class NewCustomerRegistration {
     //    Thread.sleep(1000);
 
     // Filling section "Your address"
-
     driver.findElement(By.id("input-company")).clear();
     driver.findElement(By.id("input-company")).sendKeys("SoftServe");
 
@@ -75,12 +98,11 @@ public class NewCustomerRegistration {
     //    Thread.sleep(1000);
 
     // Filling section "Your Password"
-
     driver.findElement(By.id("input-password")).clear();
-    driver.findElement(By.id("input-password")).sendKeys("qwerty");
+    driver.findElement(By.id("input-password")).sendKeys(System.getenv("PWD"));
 
     driver.findElement(By.id("input-confirm")).clear();
-    driver.findElement(By.id("input-confirm")).sendKeys("qwerty");
+    driver.findElement(By.id("input-confirm")).sendKeys(System.getenv("PWD"));
 
     driver.findElement(By.name("agree")).click();
 
@@ -88,29 +110,16 @@ public class NewCustomerRegistration {
 
     driver.findElement(By.name("agree")).sendKeys(Keys.ENTER);
 
-    /* WebElement element3 =
-                    driver.findElement(
-                        By.linkText("My Account")); // finds my account login
-        // page in the footer
-            System.out.println(element3.getTagName());
-    */
-    //    Thread.sleep(1000);
-
-    String created =
-        driver
-            .findElement(By.xpath("//h1[contains(text(), 'Your" + " Account Has Been Created!')]"))
+    String created = driver.findElement(By.xpath(
+            "//h1[contains(text(), 'Your Account Has Been Created!')]"))
             .getText();
     System.out.println(created);
     Assert.assertEquals(created, "Your Account Has Been Created!");
-    driver.quit();
+    Thread.sleep(2000);
   }
 
   @Test
-  public void deleteTestCustomer() throws Exception {
-    WebDriver driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.get("http://172.16.0.128/opencart/upload/admin");
+  public void deleteCustomerTest() throws Exception {
 
     driver.findElement(By.id("input-username")).click();
     driver.findElement(By.id("input-username")).clear();
@@ -118,7 +127,7 @@ public class NewCustomerRegistration {
 
     driver.findElement(By.id("input-password")).click();
     driver.findElement(By.id("input-password")).clear();
-    driver.findElement(By.id("input-password")).sendKeys("Lv459_Taqc");
+    driver.findElement(By.id("input-password")).sendKeys(System.getenv("DB_PASSWORD"));
 
     driver.findElement(By.id("input-password")).sendKeys(Keys.ENTER);
 
@@ -126,52 +135,32 @@ public class NewCustomerRegistration {
 
     driver.findElement(By.id("menu-customer")).click();
 
-    //    driver.findElement(By.xpath("//a[contains(@href,'Customers')]")).click();
+    //    driver.findElement(By.xpath("//a[contains(@href,'Customers')]"))
+    //    .click(); // also works
     driver.findElement(By.xpath("//a[.='Customers']")).click();
 
     driver.findElement(By.id("input-email")).click();
     driver.findElement(By.id("input-email")).clear();
-    driver.findElement(By.id("input-email")).sendKeys("lv459@i.ua");
-
+    driver.findElement(By.id("input-email")).sendKeys(System.getenv("USER_EMAIL"));
     driver.findElement(By.id("button-filter")).click();
-    // this edit button doesn't work. can find table, but cannot find 'Edit' button
-    //    driver.findElement(By.xpath("//table[contains(.,'lv459@i.ua')]")).click();
-    //
-    // $x("//table[contains(.,'lv459@i.ua')]//following-sibling::a[contains(data-original-title='Edit')]")
-
     driver.findElement(By.name("selected[]")).click();
-
     driver.findElement(By.className("btn-danger")).click();
 
     Thread.sleep(1000);
 
     driver.switchTo().alert().accept();
 
-    String text =
-        driver
-            .findElement(By.xpath("//*[contains(text(),'Success: You have modified customers!')]"))
+    String text =driver.findElement(By.xpath(
+            "//*[contains(text(),'Success: You have modified customers!')]"))
             .getText();
 
     System.out.println(text);
 
     Assert.assertEquals("You have modified customers!", text);
-
-    driver.quit();
   }
 
   @Test
-  public void newCustomerRegistrationOnefieldInvalid() throws Exception {
-
-    //    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    //    System.setProperty("webdriver.chrome.driver",
-    //                       this.getClass().getResource(Reg
-    //                                                   "chromedriver*").getPath());
-    //
-
-    WebDriver driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.get("http://172.16.0.128/opencart/upload/");
+  public void customerRegistrationOnefieldInvalidTest() throws Exception {
 
     driver.findElement(By.className("dropdown")).click();
     //    Thread.sleep(1000);
@@ -185,7 +174,7 @@ public class NewCustomerRegistration {
     driver.findElement(By.id("input-lastname")).sendKeys("Pe");
 
     driver.findElement(By.id("input-email")).clear();
-    driver.findElement(By.id("input-email")).sendKeys("lv459@i.ua");
+    driver.findElement(By.id("input-email")).sendKeys(System.getenv("USER_EMAIL"));
 
     driver.findElement(By.id("input-telephone")).clear();
     driver.findElement(By.id("input-telephone")).sendKeys("123");
@@ -219,12 +208,11 @@ public class NewCustomerRegistration {
     //    Thread.sleep(1000);
 
     // Filling section "Your Password"
-
     driver.findElement(By.id("input-password")).clear();
-    driver.findElement(By.id("input-password")).sendKeys("qwerty");
+    driver.findElement(By.id("input-password")).sendKeys(System.getenv("PWD"));
 
     driver.findElement(By.id("input-confirm")).clear();
-    driver.findElement(By.id("input-confirm")).sendKeys("qwerty");
+    driver.findElement(By.id("input-confirm")).sendKeys(System.getenv("PWD"));
 
     driver.findElement(By.name("agree")).click();
 
@@ -232,40 +220,13 @@ public class NewCustomerRegistration {
 
     driver.findElement(By.name("agree")).sendKeys(Keys.ENTER);
 
-    /* WebElement element3 =
-                    driver.findElement(
-                        By.linkText("My Account")); // finds my account login
-        // page in the footer
-            System.out.println(element3.getTagName());
-    */
-    //    Thread.sleep(1000);
-
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath(
-                    "//*[contains(text(),'Warning: E-Mail Address is already " + "registered!')]"))
-            .getText(),
-        "Warning: E-Mail Address is already registered!");
-
-    driver.findElements(By.className("text-danger"));
-
-    driver.quit();
+    Assert.assertEquals(driver.findElement(By.xpath(
+            "//*[contains(text(),'First Name must be between 1 and 32 characters!')]")).getText(),
+            "First Name must be between 1 and 32 characters!");
   }
 
   @Test
-  public void newCustomerRegistrationAllFieldsInvalid() throws Exception {
-
-    //    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    //    System.setProperty("webdriver.chrome.driver",
-    //                       this.getClass().getResource(Reg
-    //                                                   "chromedriver*").getPath());
-    //
-
-    WebDriver driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.get("http://172.16.0.128/opencart/upload/");
+  public void customerRegistrationAllFieldsInvalidTest() throws Exception {
 
     driver.findElement(By.className("dropdown")).click();
     //    Thread.sleep(1000);
@@ -277,85 +238,43 @@ public class NewCustomerRegistration {
 
     driver.findElement(By.name("agree")).sendKeys(Keys.ENTER);
 
-    /* WebElement element3 =
-                    driver.findElement(
-                        By.linkText("My Account")); // finds my account login
-        // page in the footer
-            System.out.println(element3.getTagName());
-    */
-    //    Thread.sleep(1000);
-
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath("//*[contains(text(),'First Name must be between 1 and 32 characters!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'First Name must be between 1 and 32 characters!')]"))
             .getText(),
         "First Name must be between 1 and 32 characters!");
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath(
-                    "//*[contains(text(),'Last Name must be between 1 and 32 " + "characters!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'Last Name must be between 1 and 32 " + "characters!')]"))
             .getText(),
         "Last Name must be between 1 and 32 characters!");
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath("//*[contains(text(),'E-Mail Address does not appear to be valid!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'E-Mail Address does not appear to be valid!')]"))
             .getText(),
         "E-Mail Address does not appear to be valid!");
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath("//*[contains(text(),'Telephone must be between 3 and 32 characters!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'Telephone must be between 3 and 32 characters!')]"))
             .getText(),
         "Telephone must be between 3 and 32 characters!");
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath("//*[contains(text(),'Address 1 must be between 3 and 128 characters!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'Address 1 must be between 3 and 128 characters!')]"))
             .getText(),
         "Address 1 must be between 3 and 128 characters!");
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath("//*[contains(text(),'City must be between 2 and 128 characters!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'City must be between 2 and 128 characters!')]"))
             .getText(),
         "City must be between 2 and 128 characters!");
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath("//*[contains(text(),'Postcode must be between 2 and 10 characters!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'Postcode must be between 2 and 10 characters!')]"))
             .getText(),
         "Postcode must be between 2 and 10 characters!");
-    Assert.assertEquals(
-        driver
-            .findElement(By.xpath("//*[contains(text(),'Please select a region / state!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'Please select a region / state!')]"))
             .getText(),
         "Please select a region / state!");
-    Assert.assertEquals(
-        driver
-            .findElement(
-                By.xpath("//*[contains(text(),'Password must be between 4 and 20 characters!')]"))
+    Assert.assertEquals(driver.findElement(By.xpath("//*[contains(text(),'Password must be between 4 and 20 characters!')]"))
             .getText(),
         "Password must be between 4 and 20 characters!");
 
-    driver.findElements(By.className("text-danger"));
+    List<WebElement> list = driver.findElements(By.className("text-danger"));
+
+    for (WebElement current : list) {
+      System.out.println(current.getText());
+    }
   }
 
   @Test
-  public void newCustomerEmailField() throws Exception {
-
-    //    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    //    System.setProperty("webdriver.chrome.driver",
-    //                       this.getClass().getResource(Reg
-    //                                                   "chromedriver*").getPath());
-    //
-
-    WebDriver driver = new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.get("http://172.16.0.128/opencart/upload/");
+  public void customerCreateEmailFieldTest() throws Exception {
 
     driver.findElement(By.className("dropdown")).click();
     driver.findElement(By.linkText("Register")).click();
@@ -391,7 +310,6 @@ public class NewCustomerRegistration {
             .getText(),
         "E-Mail Address does not appear to be valid!");
 
-
     driver.findElement(By.id("input-email")).clear();
     driver.findElement(By.id("input-email")).sendKeys("a@a.");
     if (!driver.findElement(By.name("agree")).isSelected()) {
@@ -405,7 +323,6 @@ public class NewCustomerRegistration {
             .getText(),
         "E-Mail Address does not appear to be valid!");
 
-
     driver.findElement(By.id("input-email")).clear();
     driver.findElement(By.id("input-email")).sendKeys("a@a.a");
     if (!driver.findElement(By.name("agree")).isSelected()) {
@@ -413,15 +330,7 @@ public class NewCustomerRegistration {
     }
     driver.findElement(By.name("agree")).sendKeys(Keys.ENTER);
 
-    Assert.assertEquals(driver.findElements(By.xpath(
-            "//*[contains(text(),'E-Mail Address does not appear to be " +
-            "valid!')]")).size(), 0);
-  }
-
-  @Test
-    public void sytem ()
-  {
-      String u = System.getenv("DB_USERNAME");
-    System.out.println(u);
+    Assert.assertEquals(driver.findElements(
+            By.xpath("//*[contains(text(),'E-Mail Address does not appear to be valid!')]")).size(),0);
   }
 }
