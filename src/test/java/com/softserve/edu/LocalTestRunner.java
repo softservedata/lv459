386de44ac813857caa0906cd026d02d1a6b739a9
Yulian <julianshutiak@gmail.com>
2019-12-18 23:58:27 +0200

    Added LocalTestRunner class

---
+++ src/test/java/com/softserve/edu/LocalTestRunner.java
@@ -0,0 +1,47 @@
+package com.softserve.edu;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public abstract class LocalTestRunner {
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        System.out.println("@BeforeClass");
+        System.setProperty("webdriver.chrome.driver",
+                LocalTestRunner.class.getResource("/chromedriver-windows-32bit.exe").getPath());
+        System.out.println("PATH: " + LocalTestRunner.class.getResource("/chromedriver-windows-32bit.exe").getPath());
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        System.out.println("@AfterClass");
+        Thread.sleep(1000); // For Presentation Only
+        driver.quit();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("\t@Before method");
+        //driver.get("http://192.168.216.128/opencart/upload/");
+        driver.get("http://taqc-opencart.epizy.com/index.php?route=common/home");
+        Thread.sleep(1000); // For Presentation Only
+        //driver.manage().window().maximize();
+        Thread.sleep(1000); // For Presentation Only
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("\t@After method");
+    }
+}
--- src/test/java/com/softserve/edu/SearchItemsTest.java
+++ src/test/java/com/softserve/edu/SearchItemsTest.java
@@ -10,26 +10,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SearchItemsTest {
+public class SearchItemsTest extends LocalTestRunner {
 
     //@Test
     public void findMacBook() throws Exception {
-        System.setProperty("webdriver.chrome.driver",
-                this.getClass().getResource("/chromedriver-windows-32bit.exe").getPath());
-        WebDriver driver = new ChromeDriver();
-        //
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://192.168.216.128/opencart/upload/");
-        //driver.get("http://taqc-opencart.epizy.com/index.php?route=common/home");
-        //
+        // Steps
+        // Typing in the "Search field"
         driver.findElement(By.name("search")).click();
         driver.findElement(By.name("search")).clear();
-        driver.findElement(By.name("search")).sendKeys("MacBook" + Keys.ENTER);
-
-        WebElement actual = driver.findElement(By.xpath("//*[@id='content']/label"));
-        Assert.assertEquals("Search Criteria", actual.getText());
-        Thread.sleep(2000);// For Presentation Only
-        driver.quit();
+        driver.findElement(By.name("search")).sendKeys("MacBook");
+        //
+        // Clicking on the "Search Button"
+        driver.findElement(By.cssSelector("button.btn.btn-default.btn-lg")).click();
+        Thread.sleep(1000); // For Presentation Only
+        //
+        // Checking
+        WebElement macBook = driver.findElement(By.xpath("//a[text()='MacBook']"));
+        Assert.assertTrue(macBook.getText().contains("MacBook"));
+        //
+        // Checking
+        WebElement macBookAir = driver.findElement(By.xpath("//a[text()='MacBook Air']"));
+        Assert.assertTrue(macBookAir.getText().contains("MacBook Air"));
+        //
+        // Checking
+        WebElement macBookPro = driver.findElement(By.xpath("//a[text()='MacBook Pro']"));
+        Assert.assertTrue(macBookPro.getText().contains("MacBook Pro"));
+        //
+        // Return to Previous State
+        driver.findElement(By.cssSelector("#logo .img-responsive")).click();
+        Thread.sleep(1000); // For Presentation Only
     }
     //@Test
     public void testAsterisk() throws Exception {
@@ -110,7 +119,7 @@ public class SearchItemsTest {
         driver.quit();
     }
 
-    @Test
+    //@Test
     public void testMacBook() throws Exception {
         System.setProperty("webdriver.chrome.driver",
                 this.getClass().getResource("/chromedriver-windows-32bit.exe").getPath());
@@ -128,5 +137,46 @@ public class SearchItemsTest {
         Assert.assertEquals("Search Criteria", actual.getText());
         Thread.sleep(2000);// For Presentation Only
         driver.quit();
+    }
+
+    //@Test
+    public void fieldLengthCaseOne() throws Exception {
+        // Steps
+        // Typing in the "Search field"
+        driver.findElement(By.name("search")).click();
+        driver.findElement(By.name("search")).clear();
+        driver.findElement(By.name("search")).sendKeys("");
+        //
+        // Clicking on the "Search Button"
+        driver.findElement(By.cssSelector("button.btn.btn-default.btn-lg")).click();
+        Thread.sleep(1000); // For Presentation Only
+        //
+        // Checking
+        WebElement zeroLength = driver.findElement(By.xpath("//input[@id='button-search']/following-sibling::p"));
+        Assert.assertTrue(zeroLength.getText().contains("There is no product that matches the search criteria."));
+        //
+        // Return to Previous State
+        driver.findElement(By.cssSelector("#logo .img-responsive")).click();
+        Thread.sleep(1000); // For Presentation Only
+    }
+    //@Test
+    public void fieldLengthCaseTwo() throws Exception {
+        // Steps
+        // Typing in the "Search field"
+        driver.findElement(By.name("search")).click();
+        driver.findElement(By.name("search")).clear();
+        driver.findElement(By.name("search")).sendKeys("z");
+        //
+        // Clicking on the "Search Button"
+        driver.findElement(By.cssSelector("button.btn.btn-default.btn-lg")).click();
+        Thread.sleep(1000); // For Presentation Only
+        //
+        // Checking
+        WebElement zeroLength = driver.findElement(By.xpath("//input[@id='button-search']/following-sibling::p"));
+        Assert.assertTrue(zeroLength.getText().contains("There is no product that matches the search criteria."));
+        //
+        // Return to Previous State
+        driver.findElement(By.cssSelector("#logo .img-responsive")).click();
+        Thread.sleep(1000); // For Presentation Only
     }
 }
