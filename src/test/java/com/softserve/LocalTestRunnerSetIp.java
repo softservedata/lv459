3b1d8cd3aee0d25f468fdf863be63b3766464069
taliana-slava <taliana.slava@gmail.com>
2019-12-19 09:54:52 +0200

    Added LocalTest file

--- src/test/java/com/softserve/LocalTestRunnerSetIp.java
+++ src/test/java/com/softserve/LocalTestRunnerSetIp.java
@@ -3,11 +3,11 @@ package com.softserve;
 import org.openqa.selenium.WebDriver;
 
 public abstract class LocalTestRunnerSetIp {
-    protected static final String IP = "172.16.0.128";
+    protected static final String IP = "192.168.234.129";
     protected static final int DELAY = 1000;
     protected static WebDriver driver;
-    protected static final String USER_EMAIL = System.getenv("USER_EMAIL");
-    protected static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
-    protected static final String USER_PASSWORD = System.getenv("USER_PASSWORD");
+    protected static final String USER_EMAIL = "lv459@i.ua";
+    protected static final String DB_PASSWORD = "Lv459_Taqc";
+    protected static final String USER_PASSWORD = "Lv459_Taqc";
 
 }
