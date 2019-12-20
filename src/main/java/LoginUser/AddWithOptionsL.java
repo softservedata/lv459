b2d1a14b9833f423f8447a605b019eaff40f2262
taliana-slava <taliana.slava@gmail.com>
2019-12-20 17:08:25 +0200

    Modified AddWithOptionsL test

--- src/main/java/LoginUser/AddWithOptionsL.java
+++ src/main/java/LoginUser/AddWithOptionsL.java
@@ -1,21 +1,35 @@
 package LoginUser;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
-import java.util.concurrent.TimeUnit;
 
 public class AddWithOptionsL extends LocalTestRunner {
     private final String Search_Field = "//input[@name='search']";
     private final String Search_Button = "//button[@class='btn btn-default btn-lg']";
     private final String ADD_TO_CART_BY_STARTS_WITH = "//a[starts-with(.,'%s')]/../../following-sibling::div/button[contains(@onclick, 'cart.add')]";
+    private final String CHOOSE_RADIO_BUTTON = "//div[@id='product']//div[@class='radio']/label[text()[contains(., '%s')]]";
+    private final String CHOOSE_CHECKBOX_BUTTON = "//div[@id='product']//div[@class='checkbox']/label[text()[contains(., '%s')]]";
+    private final String SELECT_OPTIONS = "//option[starts-with(.,'%s')]";
+    private final String INPUT_DATE = "//div[@class='input-group date']/input";
+    private final String INPUT_TIME = "//div[@class='input-group time']/input";
+    private final String INPUT_DATETIME = "//div[@class='input-group datetime']/input";
 
+
+    /**
+     * Method where we add Apple Cinema - the product which we can add to cart chosen the special
+     * options previously.
+     *
+     *Positive test
+     * @throws InterruptedException for Thread.sleep.
+     * @throws AWTException for Robot.
+     */
     @Test
     public void addWithOptionsL() throws InterruptedException, AWTException {
 
@@ -36,15 +50,35 @@ public class AddWithOptionsL extends LocalTestRunner {
         //Add Apple Cinema to Cart
         driver.findElement(By.xpath(String.format(ADD_TO_CART_BY_STARTS_WITH, "Apple Cinema"))).click();
         Thread.sleep(1000);
+        //Choose radio button which contains text "Large"
+        driver.findElement(By.xpath(String.format(CHOOSE_RADIO_BUTTON, "Large"))).click();
 
-        driver.findElement(By.xpath("//*[@id='content']/div[2]/div[3]/div/div[2]/h4/a")).click();
-        driver.findElement(By.xpath("//*[@id='input-option218']/div[1]/label/input")).click();
-        driver.findElement(By.xpath("//*[@id='input-option223']/div[1]/label/input")).click();
-        driver.findElement(By.xpath("//*[@id='input-option223']/div[2]/label/input")).click();
+        //Choose two Checkboxes
+        driver.findElement(By.xpath(String.format(CHOOSE_CHECKBOX_BUTTON, "Checkbox 1"))).click();
+        driver.findElement(By.xpath(String.format(CHOOSE_CHECKBOX_BUTTON, "Checkbox 3"))).click();
+
+        //Type Text to text-field
+        driver.findElement(By.xpath("//input[@placeholder='Text']")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Text']")).clear();
+        driver.findElement(By.xpath("//input[@placeholder='Text']")).sendKeys("test");
+
+        //Open Select
         driver.findElement(By.xpath("//select")).click();
-        driver.findElement(By.xpath("//option[starts-with(.,'Red')]")).click();
-        driver.findElement(By.xpath("//*[@id='input-option209']")).click(); //placeholder
-        driver.findElement(By.xpath("//*[@id='input-option209']")).sendKeys("test");
+        Thread.sleep(1000);
+
+        // Select options
+        driver.findElement(By.xpath(String.format(SELECT_OPTIONS, "Yellow"))).click();
+        Thread.sleep(1000);
+
+        //Type text in TextArea
+        driver.findElement(By.xpath("//textarea[@placeholder='Textarea']")).click();
+        driver.findElement(By.xpath("//textarea[@placeholder='Textarea']")).clear();
+        driver.findElement(By.xpath("//textarea[@placeholder='Textarea']")).sendKeys("Text");
+
+        /*
+        Here I use Robot class to control keyboard or mouse
+        to interact with OS Windows.
+        */
         Robot robot = new Robot();
         driver.findElement(By.xpath("//*[@id='button-upload222']/i")).click();//starts-with id text
         robot.setAutoDelay(2000);
@@ -65,7 +99,49 @@ public class AddWithOptionsL extends LocalTestRunner {
 
         Alert alert_box = driver.switchTo().alert();
         alert_box.accept();
+        Thread.sleep(1000);
+
+        /*
+            I can use this way to upload file. If I know that Upload button work correctly
+            I can put file's control sum to input attribute 'value'. But it is can be
+            used even when we tested it before and know value exactly.
+
+        WebElement uploadInput = driver.findElement(By.xpath("//button[starts-with(@id,'button-upload')]/following-sibling::input"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute(arguments[1], arguments[2]);",
+                uploadInput, "value", "fee9e81729574a48e990d2a6fcfd01c98be0c8e7");*/
+
+        // Type Date
+        driver.findElement(By.xpath(INPUT_DATE)).click();
+        driver.findElement(By.xpath(INPUT_DATE)).clear();
+        driver.findElement(By.xpath(INPUT_DATE)).sendKeys("2019-12-27");
+        Thread.sleep(1000);
+
+        // Type Time
+        driver.findElement(By.xpath(INPUT_TIME)).click();
+        driver.findElement(By.xpath(INPUT_TIME)).clear();
+        driver.findElement(By.xpath(INPUT_TIME)).sendKeys("10:50");
+        Thread.sleep(1000);
+
+        // Type Date-Time
+        driver.findElement(By.xpath(INPUT_DATETIME)).click();
+        driver.findElement(By.xpath(INPUT_DATETIME)).clear();
+        driver.findElement(By.xpath(INPUT_DATETIME)).sendKeys("2019-12-27 10:50");
+        Thread.sleep(1000);
+
+        // Type quantity
+        driver.findElement(By.id("input-quantity")).click();
+        driver.findElement(By.id("input-quantity")).clear();
+        driver.findElement(By.id("input-quantity")).sendKeys("3");
+        Thread.sleep(1000);
+
+        // Click Add to Cart Button
         driver.findElement(By.id("button-cart")).click();
+        Thread.sleep(10000);
+
+        //Check by message does product successfully are added to Cart.
+        WebElement alertMessage = driver.findElement(By.xpath("//div[@class='alert alert-success']"));
+        Assert.assertTrue(alertMessage.getText().contains("Success: You have added"));
+        Thread.sleep(1000);
 
     }
 
