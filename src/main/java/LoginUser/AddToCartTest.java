29cfcd3c96218f734d48fe72f44a838bf4347799
taliana-slava <taliana.slava@gmail.com>
2019-12-23 11:29:28 +0200

    Added AddToCartTest & deleted not needed

--- src/main/java/LoginUser/AddToCartTest.java
+++ src/main/java/LoginUser/AddToCartTest.java
@@ -5,22 +5,102 @@ import org.junit.Test;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
-public class AddWithOptionsL extends LocalTestRunner {
+public class AddToCartTest extends LocalTestRunner{
+
+    private final String Add_To_Cart_From_Home_Page =
+            "//a[text()='%s']/../../following-sibling::div/button[contains(@onclick, 'cart.add')]";
     private final String Search_Field = "//input[@name='search']";
     private final String Search_Button = "//button[@class='btn btn-default btn-lg']";
-    private final String ADD_TO_CART_BY_STARTS_WITH = "//a[starts-with(.,'%s')]/../../following-sibling::div/button[contains(@onclick, 'cart.add')]";
-    private final String CHOOSE_RADIO_BUTTON = "//div[@id='product']//div[@class='radio']/label[text()[contains(., '%s')]]";
-    private final String CHOOSE_CHECKBOX_BUTTON = "//div[@id='product']//div[@class='checkbox']/label[text()[contains(., '%s')]]";
-    private final String SELECT_OPTIONS = "//option[starts-with(.,'%s')]";
+    private final String DROP_DOWN_CART_BUTTON = "//span[@id='cart-total']";
+    private final String DELETE_ON_DROPDOWN_MENU = "//table[@class='table table-striped']//tbody//tr//a[contains(.,'%s')]/../following-sibling::td//i[@class='fa fa-times']";
     private final String INPUT_DATE = "//div[@class='input-group date']/input";
     private final String INPUT_TIME = "//div[@class='input-group time']/input";
     private final String INPUT_DATETIME = "//div[@class='input-group datetime']/input";
-    private final String DROP_DOWN_CART_BUTTON = "//span[@id='cart-total']";
-    private final String DELETE_ON_DROPDOWN_MENU = "//table[@class='table table-striped']//tbody//tr//a[contains(.,'%s')]/../following-sibling::td//i[@class='fa fa-times']";
+    private final String CHOOSE_RADIO_BUTTON = "//div[@id='product']//div[@class='radio']/label[text()[contains(., '%s')]]";
+    private final String CHOOSE_CHECKBOX_BUTTON = "//div[@id='product']//div[@class='checkbox']/label[text()[contains(., '%s')]]";
+    private final String SELECT_OPTIONS = "//option[starts-with(.,'%s')]";
+    private final String ADD_TO_CART_BY_STARTS_WITH = "//a[starts-with(.,'%s')]/../../following-sibling::div/button[contains(@onclick, 'cart.add')]";
+
+    /**
+     * Method add product to Cart from Home Page
+     *
+     * Positive test
+     * @throws InterruptedException
+     */
+    @Test
+    public void addingQuickL() throws InterruptedException {
+
+        // Goto Home Page
+        driver.findElement(By.cssSelector("#logo .img-responsive")).click();
+
+        // Find product by typing 'mac' in search field
+        driver.findElement(By.xpath(Search_Field)).click();
+        driver.findElement(By.xpath(Search_Field)).clear();
+        driver.findElement(By.xpath(Search_Field)).sendKeys("mac");
+        Thread.sleep(1000);
+
+        // Click Search Button
+        driver.findElement(By.xpath(Search_Button)).click();
+        Thread.sleep(1000);
+
+        //Add to Cart MacBook
+        driver.findElement(By.xpath(String.format(Add_To_Cart_From_Home_Page, "MacBook"))).click();
+
+        //Check by message does product successfully are added to Cart.
+        WebElement alertMessage = driver.findElement(By.xpath("//div[@class='alert alert-success']"));
+        Assert.assertTrue(alertMessage.getText().contains("Success: You have added"));
+        Thread.sleep(1000);
+
+        //Open Cart by Drop Down Menu
+        driver.findElement(By.xpath(DROP_DOWN_CART_BUTTON)).click();
+        Thread.sleep(1000);
+        //Delete product
+        driver.findElement(By.xpath(String.format(DELETE_ON_DROPDOWN_MENU,"MacBook"))).click();
+        Thread.sleep(1000);
+
+    }
+
+    /**
+     * Method Add product from specific product page.
+     * @throws InterruptedException
+     */
+    @Test
+    public void addOnProductPageL() throws InterruptedException {
+
+        // Goto Home Page
+        driver.findElement(By.className("img-responsive")).click();
+
+        // Find product by typing 'mac' in search field
+        driver.findElement(By.xpath(Search_Field)).click();
+        driver.findElement(By.xpath(Search_Field)).clear();
+        driver.findElement(By.xpath(Search_Field)).sendKeys("mac");
+        Thread.sleep(1000);
+
+        // Click Search Button
+        driver.findElement(By.xpath(Search_Button)).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//div[contains(@class,'caption')]//a[text()='MacBook']")).click();
+        driver.findElement(By.id("button-cart")).click();
+        //driver.findElement(By.className("img-responsive")).click();
+
+        WebElement alertMessage = driver.findElement(By.xpath("//div[@class='alert alert-success']"));
+        Assert.assertTrue(alertMessage.getText().contains("Success: You have added"));
+        Thread.sleep(1000);
+
+        //Open Cart by Drop Down Menu
+        driver.findElement(By.xpath(DROP_DOWN_CART_BUTTON)).click();
+        Thread.sleep(1000);
+        //Delete product
+        driver.findElement(By.xpath(String.format(DELETE_ON_DROPDOWN_MENU,"MacBook"))).click();
+        Thread.sleep(1000);
+
+    }
 
 
     /**
@@ -148,16 +228,7 @@ public class AddWithOptionsL extends LocalTestRunner {
         driver.findElement(By.xpath(DROP_DOWN_CART_BUTTON)).click();
         Thread.sleep(1000);
         //Delete product
-        driver.findElement(By.xpath(String.format(DELETE_ON_DROPDOWN_MENU,"MacBook"))).click();
-        Thread.sleep(1000);
-
-        //Open Cart by Drop Down Menu again to check is it empty
-        driver.findElement(By.xpath(DROP_DOWN_CART_BUTTON)).click();
-        Thread.sleep(1000);
-
-        //Check is Cart empty
-        WebElement alertMessage1 = driver.findElement(By.xpath("//ul[@class='dropdown-menu pull-right']//p"));
-        Assert.assertTrue(alertMessage1.getText().contains("Your shopping cart is empty!"));
+        driver.findElement(By.xpath(String.format(DELETE_ON_DROPDOWN_MENU,"Apple Cinema"))).click();
         Thread.sleep(1000);
 
     }
