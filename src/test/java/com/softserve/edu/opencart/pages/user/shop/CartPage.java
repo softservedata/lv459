32d741a611b46f61a2a3efce080f2a7466abd217
taliana-slava <taliana.slava@gmail.com>
2020-01-08 08:55:06 +0200

    Added package shop

---
+++ src/test/java/com/softserve/edu/opencart/pages/user/shop/CartPage.java
@@ -0,0 +1,21 @@
+package com.softserve.edu.opencart.pages.user.shop;
+
+import com.softserve.edu.opencart.pages.user.common.BreadCrumbPart;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage extends BreadCrumbPart {
+    public CartPage(WebDriver driver) {
+        super(driver);
+        initElements();
+    }
+
+    private void initElements(){
+
+    }
+
+    //Page Object
+
+    //Functional
+
+    //Business logic
+}
---
+++ src/test/java/com/softserve/edu/opencart/pages/user/shop/ProductCartContainerComponent.java
@@ -0,0 +1,4 @@
+package com.softserve.edu.opencart.pages.user.shop;
+
+public class ProductCartContainerComponent {
+}
