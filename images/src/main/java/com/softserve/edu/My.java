bd492848e2008f0ccb1d2208fcb80cba7b6c1b29
AndriiHaliatovskyi <Andry1a294@gmail.com>
2020-01-10 10:17:34 +0200

    deleted unnecessary stuff

--- images/src/main/java/com/softserve/edu/My.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.edu;
-
-public class My {
-	public static void main(String[] args) {
-		byte i = 56;
-		System.out.println("int i=" + i);
-		System.out.println("char i=" + (char) i);
-	}
-}
