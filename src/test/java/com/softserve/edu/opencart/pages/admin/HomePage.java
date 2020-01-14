350d4ddaf8edd7e69800c5029115ac2dd3ffce75
Serhiy Perepelyuk <3774217@gmail.com>
2020-01-15 01:44:34 +0200

    burgerMenu expands!!!

--- src/test/java/com/softserve/edu/opencart/pages/admin/HomePage.java
+++ src/test/java/com/softserve/edu/opencart/pages/admin/HomePage.java
@@ -1,21 +1,37 @@
 package com.softserve.edu.opencart.pages.admin;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-public class HomePage extends MenuPart{
+public class HomePage extends MenuPart {
+
+    private BurgerMenuComponent burgerMenuComponent;
+    private WebElement header;
+    private WebElement homePageContent;
+    private WebElement footer;
 
     public HomePage(WebDriver driver) {
         super(driver);
         initElements();
     }
+
     private void initElements() {
         // init elements
+
+        burgerMenuComponent = new BurgerMenuComponent(driver);
+        header = driver.findElement(By.cssSelector(".page-header"));
+        homePageContent = driver.findElement(By.cssSelector("#content>.container-fluid")); // contains below elements:
+        //        1чотири вікна квадрати
+        //        2карта світу + аналітика
+        //        3активність і останні замовлення
+
+        footer = driver.findElement(By.id("footer"));
+
+
     }
 
     // Page Object
-
-    // firstNameField
-  
 
     // Functional
 
---
+++ src/test/java/com/softserve/edu/opencart/tests/AdminPageTest.java
@@ -0,0 +1,16 @@
+package com.softserve.edu.opencart.tests;
+
+import com.softserve.edu.opencart.data.AdminRepo;
+import com.softserve.edu.opencart.pages.admin.HomePage;
+import org.testng.annotations.Test;
+
+public class AdminPageTest extends LocalAdminTestRunner {
+
+    @Test
+    public void sampleTest() {
+        HomePage homepage = loadApplication().successfulLogin(AdminRepo.getValidAdmin());
+
+        presentationSleep(5);
+
+    }
+}
