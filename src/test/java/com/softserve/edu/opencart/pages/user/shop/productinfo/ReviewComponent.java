44250a5d6ee7918ac742de4ff7c273584e273f52
taliana-slava <taliana.slava@gmail.com>
2020-01-16 14:28:01 +0200

    Change ProductInfoPage name

--- src/test/java/com/softserve/edu/opencart/pages/user/shop/productinfo/ReviewComponent.java
+++ src/test/java/com/softserve/edu/opencart/pages/user/shop/productinfo/ReviewComponent.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class ReviewComponent extends ProductInfoPage{
+public class ReviewComponent extends ProductInfoAppleCinemaPage {
 
 
     private WebElement Review;
