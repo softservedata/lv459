86df03bb9a68e7d86b7f989d12b66b227e73fe86
serhiy2000 <3774217@gmail.com>
2020-01-16 13:53:11 +0200

    Admin pass pf admin page updated. in AdminRepo

--- src/test/java/com/softserve/edu/opencart/data/AdminRepo.java
+++ src/test/java/com/softserve/edu/opencart/data/AdminRepo.java
@@ -1,8 +1,9 @@
 package com.softserve.edu.opencart.data;
 
 public class AdminRepo {
+    protected static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Admin getValidAdmin () {
-        return new Admin("admin", "");
+        return new Admin("admin", DB_PASSWORD);
     }
 }
