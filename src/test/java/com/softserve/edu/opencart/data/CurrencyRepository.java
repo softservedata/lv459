2132c9d525472ec8f162f0e34d19d412385188fc
“Yurii <spirnyak2010@gmail.com>
2020-01-16 23:18:02 +0200

    Added Currency and its repository to data package

---
+++ src/test/java/com/softserve/edu/opencart/data/CurrencyRepository.java
@@ -0,0 +1,34 @@
+package com.softserve.edu.opencart.data;
+
+public class CurrencyRepository {
+    private static volatile CurrencyRepository instance = null;
+
+    private CurrencyRepository() {
+    }
+
+    public static CurrencyRepository get() {
+        if (instance == null) {
+            synchronized (CurrencyRepository.class) {
+                if (instance == null) {
+                    instance = new CurrencyRepository();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public ICurrency getDefault() {
+        return getNewUACurrency();
+
+    }
+
+    public ICurrency getNewUACurrency() {
+        return Currency.get()
+                .setCurrencyTitle("Ukrainian UAH")
+                .setCurrencyCode("UAH")
+                .setCurrencyDecimalPlaces(2)
+                .setCurrencyValue(25)
+                .setCurrencyStatus("Enabled")
+                .build();
+    }
+}
