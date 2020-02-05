985644982542bd0506b3e9def35397ae29ad6b6e
Yulian <julianshutiak@gmail.com>
2020-02-06 00:21:08 +0200

    Added asserts

--- src/test/java/com/softserve/edu/rest/test/item/CreateUpdateItemsTest.java
+++ src/test/java/com/softserve/edu/rest/test/item/CreateUpdateItemsTest.java
@@ -5,7 +5,6 @@ import com.softserve.edu.rest.data.ItemRepository;
 import com.softserve.edu.rest.data.User;
 import com.softserve.edu.rest.data.UserRepository;
 import com.softserve.edu.rest.services.AdminService;
-import com.softserve.edu.rest.services.UserService;
 import com.softserve.edu.rest.test.RestTestRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,36 +27,37 @@ public class CreateUpdateItemsTest extends RestTestRunner {
     public void verifyIsItemAdded(User admin, Item book, Item copybook, Item book2, Item copybook2) {
         //
         // Step
+        // Adding items "book" and "copybook"
         AdminService addItems = loadApplication()
                 .resetServiceToInitialState()
-                .successfulAdminLogin(admin);
-        String s = addItems.addItem(book).getUserItem(book);
-        Assert.assertEquals(s, book.getItemText());
-            /*    .addItem(copybook)
+                .successfulAdminLogin(admin)
+                .addItem(book)
+                .addItem(copybook)
                 .getAllItems();
-        //Assert.assertTrue(addItems.toString().equals(UserRepository.getAdmin1().toString()));
-        // TODO Asserts
 
+        Assert.assertEquals(addItems.getUserItem(book), book.getItemText());
+        Assert.assertEquals(addItems.getUserItem(copybook), copybook.getItemText());
 
+        // Updating items "book" to "book2" and "copybook" to "copybook2"
         AdminService updateItems = addItems
                 .updateItem(book, book2)
                 .updateItem(copybook, copybook2)
                 .getAllItems();
-        // TODO Asserts
 
+        Assert.assertEquals(updateItems.getUserItem(book2), book2.getItemText());
+        Assert.assertEquals(updateItems.getUserItem(copybook2), copybook2.getItemText());
+
+        // Updating items "book2" and "copybook2"
         AdminService deleteItems = updateItems
                 .deleteItem(book2)
                 .deleteItem(copybook2)
                 .getAllItems();
-        // TODO Asserts
-*/
-        //
-        // Check
-/*        logger.info("Expected result: " + UserRepository.getAdmin1());
-        logger.info("Actual result: " + adminService.toString());
-        Assert.assertTrue(adminService.toString().equals(UserRepository.getAdmin1().toString()));*/
 
-       // updateItems.logout();
+        Assert.assertEquals(deleteItems.getUserItem(book2), null);
+        Assert.assertEquals(deleteItems.getUserItem(copybook2), null);
+
+        // Logout
+        updateItems.logout();
 
     }
 }
