d3fb219e1fd33b1722991d2378d7948123610008
serhiy2000 <3774217@gmail.com>
2020-02-10 14:18:58 +0200

    trying Chrome in Jenkins. modified runners.

---
+++ src/test/java/com/softserve/edu/opencart/tests/LocalAdminRunner.java
@@ -0,0 +1,97 @@
+package com.softserve.edu.opencart.tests;
+
+import com.softserve.edu.opencart.pages.admin.account.LoginPage;
+import com.softserve.edu.opencart.pages.user.HomePage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public abstract class LocalAdminRunner {
+
+    protected final Long ONE_SECOND_DELAY = 1000L;
+    protected final String TIME_TEMPLATE = "yyyy-MM-dd_HH-mm-ss";
+    //    private final String SERVER_URL = System.getenv().get("OPENCART_URL");
+    protected final String SERVER_URL =
+            ("http://172.16.0.131/opencart/upload/");
+    protected final String SERVER_ADMIN_URL = SERVER_URL + "admin/";
+    protected final String USER_ENABLED = "1";
+    protected final String USER_DISABLED = "0";
+    protected final String EXPECTED_ERROR_MESSAGE =
+            "Warning: No match for E-Mail Address and/or Password.";
+    protected final String CUSTOMER_SUCCESSFULLY_CREATED_MESSAGE =
+            "Your Account Has Been Created!";
+    protected final String CUSTOMER_FIRSTNAME_ERROR =
+            "First Name must be between 1 and 32 characters!";
+    protected final String FIRST_NAME_AMEND = "Lv459-TAQC-Updated";
+    protected final String CUSTOMER_UPDATED_MESSAGE =
+            "Success: Your account has been successfully updated.";
+    protected WebDriver driver; // singlethread
+    protected Map<Long, WebDriver> drivers; // multithread
+
+    //    @BeforeSuite
+    //
+    //    @AfterSuite
+
+    @BeforeClass
+    public void beforeClass(ITestContext context) {
+
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void afterClass() {
+
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("@BeforeMethod working.");
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult result) throws IOException {
+        if (!result.isSuccess()) {
+            System.out.println("***Test " + result.getName() + " ERROR");
+            // Take Screenshot, save sourceCode, save to log, prepare report,
+            // Return to takePageSource(takeScreenShot());
+            // TODO take screenshot
+        }
+    }
+
+    public LoginPage loadAdminPage() {
+        System.out.println("SERVER_ADMIN_URL: " + SERVER_ADMIN_URL);
+        driver.get(SERVER_ADMIN_URL); // single thread driver
+        return new LoginPage(driver); // single thread driver
+    }
+
+    public HomePage loadMainPage() {
+        System.out.println("server url = " + SERVER_URL);
+        driver.get(SERVER_URL); // single thread driver
+        return new HomePage(driver); // single thread driver
+    }
+
+    public void presentationSleep() {
+        presentationSleep(1);
+    }
+
+    public void presentationSleep(Integer seconds) {
+        try {
+            Thread.sleep(seconds * ONE_SECOND_DELAY); // For Presentation ONLY
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/test/java/com/softserve/edu/opencart/tests/LocalAdminSingleThreadRunner.java
+++ src/test/java/com/softserve/edu/opencart/tests/LocalAdminSingleThreadRunner.java
@@ -1,62 +1,33 @@
 package com.softserve.edu.opencart.tests;
 
-import com.softserve.edu.opencart.pages.admin.account.LoginPage;
-import com.softserve.edu.opencart.pages.user.HomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestContext;
-import org.testng.ITestResult;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public abstract class LocalAdminSingleThreadRunner {
-
-    private final Long ONE_SECOND_DELAY = 1000L;
-    private final String TIME_TEMPLATE = "yyyy-MM-dd_HH-mm-ss";
-//    private final String SERVER_URL = System.getenv().get("OPENCART_URL");
-        private final String SERVER_URL = ("http://172.16.0.131/opencart/upload/");
-    private final String SERVER_ADMIN_URL = SERVER_URL + "admin/";
-    protected final String USER_ENABLED = "1";
-    protected final String USER_DISABLED = "0";
-    protected final String EXPECTED_ERROR_MESSAGE =
-            "Warning: No match for E-Mail Address and/or Password.";
-    protected final String CUSTOMER_SUCCESSFULLY_CREATED_MESSAGE =
-            "Your Account Has Been Created!";
-    protected final String CUSTOMER_FIRSTNAME_ERROR =
-            "First Name must be between 1 and 32 characters!";
-    protected final String FIRST_NAME_AMEND = "Lv459-TAQC-Updated";
-    protected final String CUSTOMER_UPDATED_MESSAGE =
-            "Success: Your account has been successfully updated.";
-    private WebDriver driver; // singlethread
-
-//    @BeforeSuite
-//
-//    @AfterSuite
+public abstract class LocalAdminSingleThreadRunner extends LocalAdminRunner {
 
     @BeforeClass
     public void beforeClass(ITestContext context) {
 
         // For single thread driver
 
-//                WebDriverManager.firefoxdriver().setup();
-//                FirefoxOptions options = new FirefoxOptions ();
-//        driver = new FirefoxDriver(options);
-//                        options.addArguments("start-maximized");
-//                        options.addArguments("--headless");
-//
-//
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions options = new ChromeOptions();
-                driver = new ChromeDriver(options);
+        //                WebDriverManager.firefoxdriver().setup();
+        //                FirefoxOptions options = new FirefoxOptions ();
+        //        driver = new FirefoxDriver(options);
+        //                        options.addArguments("start-maximized");
+        //                        options.addArguments("--headless");
+        //
+        //
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        driver = new ChromeDriver(options);
         //        options.addArguments("start-maximized");
         //        options.addArguments("--headless");
-
 
         //        options.addArguments("--no-sandbox");
         //        options.addArguments("--disable-setuid-sandbox");
@@ -76,46 +47,6 @@ public abstract class LocalAdminSingleThreadRunner {
     public void afterClass() {
         if (driver != null) {
             driver.quit();
-        }
-    }
-
-    @BeforeMethod
-    public void beforeMethod() {
-        System.out.println("@BeforeMethod working.");
-    }
-
-    @AfterMethod
-    public void afterMethod(ITestResult result) throws IOException {
-        if (!result.isSuccess()) {
-            System.out.println("***Test " + result.getName() + " ERROR");
-            // Take Screenshot, save sourceCode, save to log, prepare report,
-            // Return to takePageSource(takeScreenShot());
-            // TODO take screenshot
-        }
-    }
-
-    public LoginPage loadAdminPage() {
-        System.out.println("SERVER_ADMIN_URL: " + SERVER_ADMIN_URL);
-        driver.get(SERVER_ADMIN_URL); // single thread driver
-        return new LoginPage(driver); // single thread driver
-    }
-
-    public HomePage loadMainPage() {
-        System.out.println("server url = " + SERVER_URL);
-        driver.get(SERVER_URL); // single thread driver
-        return new HomePage(driver); // single thread driver
-    }
-
-    public void presentationSleep() {
-        presentationSleep(1);
-    }
-
-    public void presentationSleep(Integer seconds) {
-        try {
-            Thread.sleep(seconds * ONE_SECOND_DELAY); // For Presentation ONLY
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         }
     }
 
