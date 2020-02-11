b0ec282860b47aeeea05814db5da88c0ef592127
serhiy2000 <3774217@gmail.com>
2020-02-11 09:48:41 +0200

    all runners in one folder

--- src/test/java/com/softserve/edu/opencart/tests/runner/Runner.java
+++ src/test/java/com/softserve/edu/opencart/tests/runner/Runner.java
@@ -1,30 +1,18 @@
 package com.softserve.edu.opencart.tests.runner;
 
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
-import com.softserve.edu.opencart.pages.admin.account.LoginPage;
-import com.softserve.edu.opencart.pages.user.HomePage;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.Map;
 
 public abstract class Runner {
     protected WebDriver driver; // singlethread
     protected Map<Long, WebDriver> drivers; // multithread
     protected final Long ONE_SECOND_DELAY = 1000L;
     protected final String TIME_TEMPLATE = "yyyy-MM-dd_HH-mm-ss";
-    protected final String SERVER_ADMIN_URL = SERVER_URL + "admin/";
     //    protected final String SERVER_URL = ("http://192.168.196.129/opencart/upload/");
     //    private final String SERVER_URL = System.getenv().get("OPENCART_URL");
     protected final String SERVER_URL = ("http://172.16.0.131/opencart/upload/");
+    protected final String SERVER_ADMIN_URL = SERVER_URL + "admin/";
     protected final String USER_ENABLED = "1";
     protected final String USER_DISABLED = "0";
     protected final String EXPECTED_ERROR_MESSAGE = "Warning: No match for E-Mail Address and/or Password.";
